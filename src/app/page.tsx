e92557372709354f1cd163be5fe47c31f988da7a
Jaine <jaineee00@gmail.com>
2025-03-29 12:07:56 -0400

    Add Revalidate for real time update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { getBlogPosts } from '@/app/data/notion';
 import MovieHome from './components/MovieHome'
 
+export const revalidate = 60;
+
 export default async function Home() {
   const posts = await getBlogPosts();
 
